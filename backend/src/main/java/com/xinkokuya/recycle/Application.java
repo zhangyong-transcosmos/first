7777a96f7e172354b95f260dab9248be661ab7ee
zhangyong-transcosmos <zhang.yong1@trans-cosmos.com.cn>
2019-08-26 09:12:14 +0800

    modify pom

--- backend/src/main/java/com/xinkokuya/recycle/Application.java
+++ backend/src/main/java/com/xinkokuya/recycle/Application.java
@@ -2,12 +2,16 @@ package com.xinkokuya.recycle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableEncryptableProperties
-public class Application {
+public class Application extends SpringBootServletInitializer {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
